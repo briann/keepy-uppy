9add70615ee03de8f0b96553e3849671eb05cb05
Brian Ngo <brian@bngo.dev>
2023-12-12 14:54:16 +0800

    Fix balloon math

--- src/KeepyUppy.sol
+++ src/KeepyUppy.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.13;
 
 contract KeepyUppy {
-    uint256 public constant ACCELERATION_GWEI_PER_BLOCK = 10;
+    uint256 public constant ACCELERATION_WEI_PER_BLOCK = 10;
 
     address public owner;
 
     // Game state
     address public lastBumper;
     uint256 public lastBumpBlockNumber;
+    uint256 public lastUpdateBlockNumber;
     uint256 public velocity = 0;
     uint256 public balloonHeight = 0;
 
@@ -25,16 +26,32 @@ contract KeepyUppy {
         require(msg.value > 0);
         lastBumper = msg.sender;
         lastBumpBlockNumber = block.number;
+        lastUpdateBlockNumber = block.number;
+        // Instantly transport the balloon into the "air".
+        velocity = 0;
         balloonHeight = msg.value;
     }
 
     function updateState() external onlyOwner {
-        velocity = (block.number - lastBumpBlockNumber) * ACCELERATION_GWEI_PER_BLOCK;
-        if (velocity > balloonHeight) {
+        (uint256 fallDistance, uint256 newVelocity) = calculateFallDistanceAndNewVelocity(
+            velocity, block.number - lastUpdateBlockNumber, ACCELERATION_WEI_PER_BLOCK
+        );
+        if (fallDistance > balloonHeight) {
             endGame();
         } else {
-            balloonHeight -= velocity;
+            lastUpdateBlockNumber = block.number;
+            velocity = newVelocity;
+            balloonHeight -= fallDistance;
         }
+    }
+
+    function calculateFallDistanceAndNewVelocity(uint256 initialVelocity, uint256 blocksElapsed, uint256 acceleration)
+        internal
+        pure
+        returns (uint256 fallDistance, uint256 newVelocity)
+    {
+        newVelocity = initialVelocity + (blocksElapsed * acceleration);
+        fallDistance = uint256((initialVelocity + newVelocity) * blocksElapsed / 2);
     }
 
     function endGame() internal onlyOwner {
@@ -43,9 +60,10 @@ contract KeepyUppy {
     }
 
     function resetGameState() private {
+        lastBumper = address(0);
+        lastBumpBlockNumber = 0;
+        lastUpdateBlockNumber = 0;
         velocity = 0;
         balloonHeight = 0;
-        lastBumpBlockNumber = 0;
-        lastBumper = address(0);
     }
 }
--- test/KeepyUppy.t.sol
+++ test/KeepyUppy.t.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test, console2} from "lib/forge-std/src/Test.sol";
 import {KeepyUppy} from "../src/KeepyUppy.sol";
 
 contract KeepyUppyHarness is KeepyUppy {
+    function exposed_calculateFallDistanceAndNewVelocity(
+        uint256 initialVelocity,
+        uint256 blocksElapsed,
+        uint256 acceleration
+    ) external pure returns (uint256 fallDistance, uint256 newVelocity) {
+        return calculateFallDistanceAndNewVelocity(initialVelocity, blocksElapsed, acceleration);
+    }
+
     function exposed_endGame() external {
         return endGame();
     }
@@ -55,22 +63,85 @@ contract KeepyUppyTest is Test {
         assertEq(game.lastBumpBlockNumber(), blockNumber);
     }
 
-    // TODO: Add a fuzzy version of this test.
     function test_updateState_BlockTimeMovesBalloonDown() public {
         address player = vm.addr(1);
         uint256 amount = 1 ether;
         uint256 blockNumber = playerBumpsBalloon(player, amount);
         assertEq(game.balloonHeight(), amount);
 
-        vm.roll(blockNumber + 1);
+        // 1 block elapsed since bump
+        blockNumber++;
+        vm.roll(blockNumber);
         game.updateState();
-        assertEq(game.balloonHeight(), amount - game.ACCELERATION_GWEI_PER_BLOCK());
+        assertEq(game.balloonHeight(), amount - 5);
+
+        // 2 blocks elapsed since bump
+        blockNumber++;
+        vm.roll(blockNumber);
+        game.updateState();
+        assertEq(game.balloonHeight(), amount - 20);
+
+        // 3 blocks elapsed since bump
+        blockNumber++;
+        vm.roll(blockNumber);
+        game.updateState();
+        assertEq(game.balloonHeight(), amount - 45);
+
+        // 100 blocks elapsed since bump
+        blockNumber = blockNumber + 97;
+        vm.roll(blockNumber);
+        game.updateState();
+        assertEq(game.balloonHeight(), amount - 50000);
     }
 
     function test_updateState_NonOwnerCallingShouldRevert() public {
         vm.prank(vm.addr(2));
         vm.expectRevert();
         game.updateState();
+    }
+
+    function test_calculateFallDistanceAndNewVelocity() public {
+        uint256 fallDistance;
+        uint256 newVelocity;
+
+        // Basic calculations with zero initial velocity.
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(0, 1, 9);
+        assertEq(fallDistance, 4);
+        assertEq(newVelocity, 9);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(0, 2, 9);
+        assertEq(fallDistance, 18);
+        assertEq(newVelocity, 18);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(0, 3, 9);
+        assertEq(fallDistance, 40);
+        assertEq(newVelocity, 27);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(0, 100, 9);
+        assertEq(fallDistance, 45000);
+        assertEq(newVelocity, 900);
+
+        // Calculations with some initial velocity.
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(55, 1, 9);
+        assertEq(fallDistance, 59);
+        assertEq(newVelocity, 64);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(55, 2, 9);
+        assertEq(fallDistance, 128);
+        assertEq(newVelocity, 73);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(55, 3, 9);
+        assertEq(fallDistance, 205);
+        assertEq(newVelocity, 82);
+
+        (fallDistance, newVelocity) = game.exposed_calculateFallDistanceAndNewVelocity(55, 100, 9);
+        assertEq(fallDistance, 50500);
+        assertEq(newVelocity, 955);
+    }
+
+    function test_calculateFallDistanceAndNewVelocity_WithOverflowSituations() public {
+        // TODO
     }
 
     function test_endGame_PaysOutAndResetsState() public {
