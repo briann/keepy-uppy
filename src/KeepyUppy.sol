69df2bd821700f20577e28b2175fbc95c93bb89c
Brian Ngo <brian@bngo.dev>
2023-12-12 12:46:04 +0800

    Add game state

--- src/KeepyUppy.sol
+++ src/KeepyUppy.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.13;
 
 contract KeepyUppy {
+    uint256 public constant ACCELERATION_GWEI_PER_BLOCK = 10;
 
-    address public lastBumper;
     address public owner;
+
+    // Game state
+    address public lastBumper;
+    uint256 public lastBumpBlockNumber;
+    uint256 public velocity = 0;
+    uint256 public balloonHeight = 0;
 
     constructor() {
         owner = msg.sender;
@@ -18,9 +24,28 @@ contract KeepyUppy {
     function bumpBalloon() external payable {
         require(msg.value > 0);
         lastBumper = msg.sender;
+        lastBumpBlockNumber = block.number;
+        balloonHeight = msg.value;
     }
 
-    function payout() public onlyOwner {
+    function updateState() external onlyOwner {
+        velocity = (block.number - lastBumpBlockNumber) * ACCELERATION_GWEI_PER_BLOCK;
+        if (velocity > balloonHeight) {
+            endGame();
+        } else {
+            balloonHeight -= velocity;
+        }
+    }
+
+    function endGame() internal onlyOwner {
         payable(lastBumper).transfer(address(this).balance);
+        resetGameState();
+    }
+
+    function resetGameState() private {
+        velocity = 0;
+        balloonHeight = 0;
+        lastBumpBlockNumber = 0;
+        lastBumper = address(0);
     }
 }
--- test/KeepyUppy.t.sol
+++ test/KeepyUppy.t.sol
@@ -4,47 +4,95 @@ pragma solidity ^0.8.13;
 import {Test, console2} from "forge-std/Test.sol";
 import {KeepyUppy} from "../src/KeepyUppy.sol";
 
+contract KeepyUppyHarness is KeepyUppy {
+    function exposed_endGame() external {
+        return endGame();
+    }
+}
+
 contract KeepyUppyTest is Test {
-    KeepyUppy public game;
-    address player;
+    KeepyUppyHarness public game;
 
     function setUp() public {
-        player = vm.addr(1);
-        game = new KeepyUppy();
+        game = new KeepyUppyHarness();
     }
 
-    function test_bumpBalloon() public {
-        vm.prank(player);
-        vm.deal(player, 1 ether);
-        game.bumpBalloon{value: 1 ether}();
-        assertEq(address(game).balance, 1 ether);
+    function playerBumpsBalloon(address _player, uint256 _amount) private returns (uint256) {
+        return playerBumpsBalloon(_player, _amount, 100);
     }
 
-    function testFuzz_bumpBalloon(uint256 _amount) public {
-        vm.assume(_amount > 0);
-        vm.prank(player);
-        vm.deal(player, _amount);
+    function playerBumpsBalloon(address _player, uint256 _amount, uint256 _blockNumber) private returns (uint256) {
+        vm.prank(_player);
+        vm.deal(_player, _amount);
+        vm.roll(_blockNumber);
         game.bumpBalloon{value: _amount}();
-        assertEq(address(game).balance, _amount);
+        return _blockNumber;
     }
 
-    function test_payout() public {
-        vm.prank(player);
-        vm.deal(player, 1 ether);
-        game.bumpBalloon{value: 1 ether}();
+    function test_bumpBalloon_IncreasesContractBalance() public {
+        address player = vm.addr(1);
+        uint256 amount = 1 ether;
+        uint256 blockNumber = playerBumpsBalloon(player, amount);
+        vm.roll(blockNumber + 1);
 
-        game.payout();
-        assertEq(address(game).balance, 0);
-        assertEq(player.balance, 1 ether);
+        // Asserts
+        assertEq(address(game).balance, amount);
+        assertEq(game.lastBumper(), player);
+        assertEq(game.balloonHeight(), amount);
+        assertEq(game.lastBumpBlockNumber(), blockNumber);
     }
 
-    function test_payout_onlyOwner() public {
-        vm.prank(player);
-        vm.deal(player, 1 ether);
-        game.bumpBalloon{value: 1 ether}();
+    function testFuzz_bumpBalloon_IncreasesContractBalance(uint256 amount) public {
+        vm.assume(amount > 0);
+        address player = vm.addr(1);
+        uint256 blockNumber = playerBumpsBalloon(player, amount);
+        vm.roll(blockNumber + 1);
 
+        // Asserts
+        assertEq(address(game).balance, amount);
+        assertEq(game.lastBumper(), player);
+        assertEq(game.balloonHeight(), amount);
+        assertEq(game.lastBumpBlockNumber(), blockNumber);
+    }
+
+    // TODO: Add a fuzzy version of this test.
+    function test_updateState_BlockTimeMovesBalloonDown() public {
+        address player = vm.addr(1);
+        uint256 amount = 1 ether;
+        uint256 blockNumber = playerBumpsBalloon(player, amount);
+        assertEq(game.balloonHeight(), amount);
+
+        vm.roll(blockNumber + 1);
+        game.updateState();
+        assertEq(game.balloonHeight(), amount - game.ACCELERATION_GWEI_PER_BLOCK());
+    }
+
+    function test_updateState_NonOwnerCallingShouldRevert() public {
         vm.prank(vm.addr(2));
         vm.expectRevert();
-        game.payout();
+        game.updateState();
+    }
+
+    function test_endGame_PaysOutAndResetsState() public {
+        address player = vm.addr(1);
+        uint256 amount = 1 ether;
+
+        uint256 blockNumber = playerBumpsBalloon(player, amount);
+        vm.roll(blockNumber + 1);
+        game.exposed_endGame();
+
+        // Asserts
+        assertEq(address(game).balance, 0);
+        assertEq(player.balance, 1 ether);
+        assertEq(game.velocity(), 0);
+        assertEq(game.balloonHeight(), 0);
+        assertEq(game.lastBumpBlockNumber(), 0);
+        assertEq(game.lastBumper(), address(0));
+    }
+
+    function test_endGame_NonOwnerCallingShouldRevert() public {
+        vm.prank(vm.addr(2));
+        vm.expectRevert();
+        game.exposed_endGame();
     }
 }
