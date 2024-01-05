5cf094a7cf6dfefc27382c3eb6da0ae8e1c93f24
Brian Ngo <brian@bngo.dev>
2024-01-05 22:16:55 +0800

    Update script to deploy

--- script/KeepyUppy.s.sol
+++ script/KeepyUppy.s.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console2} from "lib/forge-std/src/Script.sol";
+import {KeepyUppy} from "src/KeepyUppy.sol";
 
 contract KeepyUppyScript is Script {
-    function setUp() public {}
-
     function run() public {
-        vm.broadcast();
+        vm.broadcast(vm.envUint("PRIVATE_KEY"));
+        KeepyUppy game = new KeepyUppy(100, 10 wei);
+        console2.log("Deployed game to", address(game));
     }
 }
